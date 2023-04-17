dfde869c4b2008e6116a6efc84c3434f365aa6eb
fukuharayuu <fukuharayuu@localhost>
2023-04-17 16:38:37 +0900

    task5

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -94,7 +94,7 @@ public class BooksService {
 					bookInfo.getPublishDate(), bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
 		} else {
 			// TODO 取得した書籍情報を更新するようにSQLを修正（タスク５）
-			sql = "UPDATE books SET title=?, author=?, publisher=?, publishDate=?, thumbnailName=?, thumbnailUrl=? WHERE id = ?";
+			sql = "UPDATE books SET title=?, author=?, publisher=?, publishDate=?, thumbnailName=?, thumbnailUrl=?, isbn=?, description=? WHERE id = ?";
 			jdbcTemplate.update(sql, bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
 					bookInfo.getPublishDate(), bookInfo.getThumbnailName(), bookInfo.getThumbnailUrl(),
 					bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
