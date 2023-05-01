15fbc483e3a4bb659a4d1fda19dcfec14e38cdb9
fukuharayuu <fukuharayuu@localhost>
2023-05-01 14:18:36 +0900

    tasktest

--- src/main/java/jp/co/seattle/library/controller/PasswordResetController.java
+++ src/main/java/jp/co/seattle/library/controller/PasswordResetController.java
@@ -1,5 +1,7 @@
 package jp.co.seattle.library.controller;
 
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,30 +21,33 @@ public class PasswordResetController {
 	@Autowired
 	private UsersService usersService;
 
-	@RequestMapping(value = "/PasswordReset", method = RequestMethod.POST)
+	@RequestMapping(value = "/PasswordReset", method = RequestMethod.GET)
 	public String first(Model model) {
 		return "passwordReset";
 	}
 
-	@RequestMapping(value = "/PassWordReset", method = RequestMethod.POST)
-	public String passwordReset(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+	@RequestMapping(value = "/Reset", method = RequestMethod.POST)
+	public String createAccount(Locale locale, @RequestParam("email") String email,
+			@RequestParam("password") String password, @RequestParam("passwordForCheck") String passwordForCheck,
+			Model model) {
+		// デバッグ用ログ
+		logger.info("Welcome passwordReset! The client locale is {}.", locale);
+
+		// バリデーションチェック、パスワード一致チェック（タスク１）
 		if (password.length() >= 8 && password.matches("^[A-Za-z0-9]+$")) {
 			if (password.equals(passwordForCheck)) {
 				UserInfo userInfo = new UserInfo();
 				userInfo.setEmail(email);
 				userInfo.setPassword(password);
-				usersService.registUser(userInfo);
-				return "redirect:/";
+				usersService.resetUser(userInfo);
 			} else {
 				model.addAttribute("errorMessage", "パスワードと確認用パスワードが一致しません。");
-				return "PasswordReset";
+				return "passwordReset";
 			}
 		} else {
-			model.addAttribute("errorMessage", "パスワードは８桁以上の半角英数字で設定してください。");
-			return "PasswordReset";
+			model.addAttribute("errorMessage", "パスワードは8文字以上かつ半角英数字に設定してください");
+			return "passwordReset";
 		}
-		//usersテーブルのパスワードを更新
-		booksService.PasswordReset(password);
-		
+		return "redirect:/";
 	}
 }
